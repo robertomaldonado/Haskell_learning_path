317131c539e6d483be69aa708bd8f52436135c86
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-10-17 14:41:30 -0500

    Calling functions from functions

--- baby.hs
+++ baby.hs
@@ -1,1 +1,7 @@
 doubleMe x = x + x
+
+doubleUs x y = x * 2 + y * 2
+
+-- As expected, you can call your own functions from other functions that you made.
+-- With that in mind, we could redefine doubleUs:
+doubleUsEnhanced x y = doubleMe x + doubleMe y
