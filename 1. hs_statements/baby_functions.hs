1fc764812a8bb520a7dd572cf24f950a43461630
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-10-24 18:05:11 -0500

    relocated files

--- 1. hs_statements/baby_functions.hs
+++ 1. hs_statements/baby_functions.hs
@@ -4,4 +4,5 @@ doubleUs x y = x * 2 + y * 2
 
 -- As expected, you can call your own functions from other functions that you made.
 -- With that in mind, we could redefine doubleUs:
+doubleUsEnhanced :: (Num a) => a -> a -> a
 doubleUsEnhanced x y = doubleMe x + doubleMe y
